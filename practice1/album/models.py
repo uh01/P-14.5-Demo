9558cbc0001c68a19a755565bf428a86a52dec36
Your Name <you@example.com>
2023-12-08 22:39:43 +0600

    14.5-Demo

---
+++ practice1/album/models.py
@@ -0,0 +1,13 @@
+# album/models.py
+from django.db import models
+from musician.models import Musician
+
+class Album(models.Model):
+    # id_no = models.IntegerField(unique=True)
+    album_name = models.CharField(max_length=100)
+    musician = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    release_date = models.DateField()
+    rating = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+
+    def __str__(self):
+        return self.album_name
